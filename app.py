4d94d6305963111965818fede2a45c0ca95ae301
HansPunsLee <lihanxuan@live.com>
2025-07-16 14:30:25 +0800

    Remove broken image display logic

--- app.py
+++ app.py
@@ -45,16 +45,16 @@ with st.sidebar.expander("About this project"):
 st.title("🛠️ Metal Extraction Cost Predictor")
 
 # Metal images hosted on GitHub or locally in repo folder 'images/'
-metal_images = {
-    "Aluminum (Al)": "https://upload.wikimedia.org/wikipedia/commons/8/85/Aluminium_eloi.jpg",
-    "Copper (Cu)": "https://upload.wikimedia.org/wikipedia/commons/1/1d/Copper_ingots_Picryl.jpg",
-    "Lithium (Li)": "https://upload.wikimedia.org/wikipedia/commons/5/59/Lithium_in_oil.jpg",
-    "Nickel (Ni)": "https://upload.wikimedia.org/wikipedia/commons/d/d3/Nickel_crystals.jpg",
-    "Zinc (Zn)": "https://upload.wikimedia.org/wikipedia/commons/5/57/Zinc_plates.jpg",
-    "Cobalt (Co)": "https://upload.wikimedia.org/wikipedia/commons/4/47/Cobalt_crystals.jpg",
-    "Platinum (Pt)": "https://upload.wikimedia.org/wikipedia/commons/5/52/Platinum_bar.jpg",
-    "Gold (Au)": "https://upload.wikimedia.org/wikipedia/commons/0/09/Gold_Nuggets.jpg"
-}
+#metal_images = {
+ #   "Aluminum (Al)": "https://upload.wikimedia.org/wikipedia/commons/8/85/Aluminium_eloi.jpg",
+  #  "Copper (Cu)": "https://upload.wikimedia.org/wikipedia/commons/1/1d/Copper_ingots_Picryl.jpg",
+   # "Lithium (Li)": "https://upload.wikimedia.org/wikipedia/commons/5/59/Lithium_in_oil.jpg",
+    #"Nickel (Ni)": "https://upload.wikimedia.org/wikipedia/commons/d/d3/Nickel_crystals.jpg",
+    #"Zinc (Zn)": "https://upload.wikimedia.org/wikipedia/commons/5/57/Zinc_plates.jpg",
+    #"Cobalt (Co)": "https://upload.wikimedia.org/wikipedia/commons/4/47/Cobalt_crystals.jpg",
+    #"Platinum (Pt)": "https://upload.wikimedia.org/wikipedia/commons/5/52/Platinum_bar.jpg",
+    #"Gold (Au)": "https://upload.wikimedia.org/wikipedia/commons/0/09/Gold_Nuggets.jpg"
+#}
 
 
 # Metal properties dictionary
@@ -129,8 +129,8 @@ metal_data = {
 metal_choice = st.selectbox("Select Metal", list(metal_data.keys()), help="Choose a metal to predict its extraction cost.")
 
 # Show metal image if available
-if metal_choice in metal_images:
-    st.image(metal_images[metal_choice], width=150, caption=metal_choice)
+#if metal_choice in metal_images:
+    #st.image(metal_images[metal_choice], width=150, caption=metal_choice)
 
 # Editable inputs with default values from dictionary
 st.write("### Adjust Metal Properties (override if desired):")
